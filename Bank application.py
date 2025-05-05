a6d7a25845f0eba5e255fe71a761970b03270c0b
Ramanathan Danuharan <UT010037@unicomtic.com>
2025-05-05 14:56:32 +0530

    first commit

---
+++ Bank application.py
@@ -0,0 +1,46 @@
+#Create an account
+ account={}
+def create_an_account():
+    name=input("Enter Account Holder Name")
+
+    account_holder_name=input("Enter The Account Holder Name")
+    if account number in account number:
+        #account_number= Entered account_number
+         print("account is not created")
+    account_number=int("Enter The Account Number")
+    if account number is valid:
+        print("Account Holder Name")
+    else:
+        print("Invalid Account Number")
+        account_number.append(account_number)
+        another_try=(input("Do You Want Another Try?"))
+        
+Account Holder Name=input("Enter Your Account Holder Name")
+         password=input("Enter Your Pass-word")
+        if Account_Number=("Enter Account Number")
+         print("Account Has Been Created")
+
+def withdrawing money():
+    input=("Enter The Amount")
+    if entered amount< Available Balance
+        print("Amount Successfully Withdrawn")
+    Else:
+        print("Enter The Choice From 1-4")
+        balance=float(input("Enter Starting Balance"))
+        
+
+
+
+
+        
+
+
+ 
+
+
+
+
+
+
+
+        
